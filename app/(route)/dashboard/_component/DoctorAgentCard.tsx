fc0e0e8b481520a9acf4748ddda032f703fbb102
sarthak1656 <sipunp70@gmail.com>
2026-02-07 18:53:53 +0530

    refactor: update DoctorAgentCard and DoctorAgentList components for consistency and improved structure 1.36

--- app/(route)/dashboard/_component/DoctorAgentCard.tsx
+++ app/(route)/dashboard/_component/DoctorAgentCard.tsx
@@ -1,4 +1,7 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
 import React from "react";
+import { ArrowRight } from "lucide-react";
 
 type doctorAgent = {
   id: number;
@@ -11,11 +14,30 @@ type doctorAgent = {
 };
 
 type props = {
-  doctor: doctorAgent;
+  doctorAgent: doctorAgent;
 };
 
-function DoctorAgentCard({ doctor }: props) {
-  return <div>DoctorAgentCard</div>;
+function DoctorAgentCard({ doctorAgent }: props) {
+  return (
+    <div className=" ">
+      <Image
+        src={doctorAgent.image}
+        alt={doctorAgent.specialist}
+        width={200}
+        height={200}
+        className=" w-full h-62.5 object-cover rounded-2xl"
+      />
+      <h2 className="font-bold ">{doctorAgent.specialist} </h2>
+      <p className="line-clamp-2 mt-1 text-sm text-gray-500">
+        {" "}
+        {doctorAgent.description}{" "}
+      </p>
+      <Button className="w-full mt-2">
+        Start Consultation
+        <ArrowRight className="ml-2" />
+      </Button>
+    </div>
+  );
 }
 
 export default DoctorAgentCard;
--- app/(route)/dashboard/_component/DoctorAgentList.tsx
+++ app/(route)/dashboard/_component/DoctorAgentList.tsx
@@ -6,10 +6,10 @@ function DoctorAgentList() {
   return (
     <div className="mt-10">
       <h2 className="font-bold text-2xl">Ai Specialist Doctor Agent</h2>
-      <div className="">
+      <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-8 mt-5">
         {AIDoctorAgents.map((doctor) => (
           <div key={doctor.id}>
-            <DoctorAgentCard doctor={doctor} />
+            <DoctorAgentCard doctorAgent={doctor} />
           </div>
         ))}
       </div>
