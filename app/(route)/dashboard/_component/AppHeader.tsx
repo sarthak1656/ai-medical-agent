2f7cd3cae7c1b6f5b7a3d9122735ac4554de0d55
sarthak1656 <sipunp70@gmail.com>
2026-02-07 12:19:49 +0530

    enhance AppHeader with menu options and user authentication button; add proxy middleware for route protection 1.19.14

--- app/(route)/dashboard/_component/AppHeader.tsx
+++ app/(route)/dashboard/_component/AppHeader.tsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import { UserButton } from "@clerk/nextjs";
+import Image from "next/image";
+import React from "react";
 
 function AppHeader() {
+  const menuOptions = [
+    {
+      id: 1,
+      name: "Home",
+      path: "/home",
+    },
+    {
+      id: 2,
+      name: "History",
+      path: "/history",
+    },
+    {
+      id: 3,
+      name: "Pricing",
+      path: "/pricing",
+    },
+    {
+      id: 4,
+      name: "Profile",
+      path: "/profile",
+    },
+  ];
   return (
-    <div>AppHeader</div>
-  )
+    <div className="flex items-center justify-between p-4 shadow px-10 md:px-20 lg:px-40" >
+      <Image src={"/logo.svg"} alt="Logo" width={180} height={90} />
+      <div className="hidden md:flex gap-12 items-center">
+        {menuOptions.map((option) => (
+          <div className="" key={option.id}>
+            <h2 className="hover:font-bold cursor-pointer transition-all">{option.name}</h2>
+          </div>
+        ))}
+      </div>
+      <UserButton />
+    </div>
+  );
 }
 
-export default AppHeader
+export default AppHeader;
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,11 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Outfit } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import Provider from "./provider";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const outfit = Outfit({
+  variable: "--font-outfit",
   subsets: ["latin"],
 });
 
@@ -27,9 +22,7 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <body
-          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-        >
+        <body className={`${outfit.variable} antialiased`}>
           <Provider>{children}</Provider>
         </body>
       </html>
